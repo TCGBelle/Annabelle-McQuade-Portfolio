38614c3667923157c4821d2ad82fde40f869be5a
TCGBelle <lumaknight@gmail.com>
2022-01-09 23:27:04 +0000

    temporary changes

--- Source/ARCourcework/ARGameInstance.cpp
+++ Source/ARCourcework/ARGameInstance.cpp
@@ -3,3 +3,39 @@
 
 #include "ARGameInstance.h"
 
+void UARGameInstance::Init()
+{
+	bScanningState = false;
+	iScanningTracking = 0;
+	bRacingState = false;
+}
+
+void UARGameInstance::SetScanningState(bool NewState)
+{
+	bScanningState = NewState;
+}
+
+bool UARGameInstance::GetScanningState() const
+{
+	return bScanningState;
+}
+
+void UARGameInstance::SetScanningTracking(int NewTracking)
+{
+	iScanningTracking = NewTracking;
+}
+
+int UARGameInstance::GetScanningTracking() const
+{
+	return iScanningTracking;
+}
+
+void UARGameInstance::SetRacingState(bool NewState)
+{
+	bRacingState = NewState;
+}
+
+bool UARGameInstance::GetRacingState() const
+{
+	return bRacingState;
+}
--- Source/ARCourcework/ARGameInstance.h
+++ Source/ARCourcework/ARGameInstance.h
@@ -13,5 +13,29 @@ UCLASS()
 class ARCOURCEWORK_API UARGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+public: 
+	UFUNCTION()
+	virtual void Init();
+
+	UFUNCTION(BlueprintCallable)
+	void SetScanningState(bool NewState);
+	UFUNCTION(BlueprintCallable)
+	bool GetScanningState() const;
+	UFUNCTION(BlueprintCallable)
+	void SetScanningTracking(int NewTracking);
+	UFUNCTION(BlueprintCallable)
+	int GetScanningTracking() const;
+	UFUNCTION(BlueprintCallable)
+	void SetRacingState(bool NewState);
+	UFUNCTION(BlueprintCallable)
+	bool GetRacingState() const;
+private:
+	UPROPERTY(BlueprintGetter = GetScanningState)
+	bool bScanningState;
+	UPROPERTY(BlueprintGetter = GetScanningTracking)
+	int iScanningTracking;
+	UPROPERTY(BlueprintGetter = GetRacingState)
+	bool bRacingState;
+
 };
